c44c1a64a148b44b34b44929e9d58a3ff242977d
PieroZ01 <piero.z.2001@gmail.com>
2024-03-16 00:00:47 +0100

    Adjusted parameter

--- exercise2/Solution/include/mandelbrot.h
+++ exercise2/Solution/include/mandelbrot.h
@@ -4,7 +4,7 @@
 // Constants
 #define XWIDTH 1000
 #define YWIDTH 1000
-#define MAXVAL 65535
+#define MAXVAL 1000
 
 // Function declarations
 void write_pgm_image( void *image, int maxval, int xsize, int ysize, const char *image_name);
