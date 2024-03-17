d12bac6bdbfbf7b9018b7e0d15fd44c476620b5f
PieroZ01 <piero.z.2001@gmail.com>
2024-03-17 21:54:56 +0100

    Small corrections 2.0

--- exercise2/Solution/include/mandelbrot.h
+++ exercise2/Solution/include/mandelbrot.h
@@ -10,10 +10,17 @@
 #define MAXVAL 65535
 
 // Function declarations
+
+// Function to write a PGM file (grey scale image)
 void write_pgm_image( void *image, int maxval, int xsize, int ysize, const char *image_name);
 
-inline double complex f_c(const double complex z, const double complex c);
+// Define the complex function f_c(z) = z^2 + c
+inline double complex f_c(const double complex z, const double complex c)
+{
+  return z*z + c;
+}
 
+// Declare the function to compute the mandelbrot set
 const int mandelbrot(const double complex c, const int max_iter);
 
 #endif // MANDELBROT_H
