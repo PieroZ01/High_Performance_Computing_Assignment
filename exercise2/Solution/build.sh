9f6382390fce935b0b940ca6631194f43036eba8
PieroZ01 <piero.z.2001@gmail.com>
2024-03-17 15:25:20 +0100

    Modified build.sh to use EPYC

--- exercise2/Solution/build.sh
+++ exercise2/Solution/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#SBATCH --job-name=compilation_ex2C
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=EPYC
+
+# set -x
+
 # Load the required modules
 module load openMPI/4.1.5/gnu/12.2.1
 module load cmake/3.28.1
@@ -7,6 +14,8 @@ module load cmake/3.28.1
 # Build the project
 cmake -S . -B build/
 make -C build/
+
+# set +x
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
