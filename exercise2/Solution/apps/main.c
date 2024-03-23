d163ff10bcf1da32769570c11ce79159ffd1ef2b
PieroZ01 <piero.z.2001@gmail.com>
2024-03-23 16:54:39 +0100

    Round robin schedule - fixing communication

--- exercise2/Solution/apps/main.c
+++ exercise2/Solution/apps/main.c
@@ -244,6 +244,16 @@ int main(int argc, char *argv[])
   // The master process writes the image to a pgm file in the build/bin directory and frees the memory
   if (rank == 0)
   {
+    // Flatten the global matrix M into a 1D array to write the image to a pgm file
+    short int *global_M_flat = (short int *)malloc(n_x * n_y * sizeof(short int));
+    for (int j = 0; j < n_y; ++j)
+    {
+      for (int i = 0; i < n_x; ++i)
+      {
+        global_M_flat[j * n_x + i] = global_M[j][i];
+      }
+    }
+
     write_pgm_image(global_M, I_max, n_x, n_y, "mandelbrot.pgm");
 
     // Free the memory for the global matrix M
