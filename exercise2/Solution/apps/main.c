46e9896f892ee4acd61aa913a2c2acbe7dce835c
PieroZ01 <piero.z.2001@gmail.com>
2024-03-23 21:46:13 +0100

    reordering

--- exercise2/Solution/apps/main.c
+++ exercise2/Solution/apps/main.c
@@ -190,13 +190,12 @@ int main(int argc, char *argv[])
       short int *reordered_M = (short int *)malloc(n_x * n_y * sizeof(short int));
       for (int r = 0; r < size; ++r)
       {
-        //const int local_rows = (r < remaining_rows) ? rows_per_process + 1 : rows_per_process;
-        const int max_rows = rows_per_process + 1;
+        const int local_rows = (r < remaining_rows) ? rows_per_process + 1 : rows_per_process;
        for (int j = 0; j < local_rows; ++j)
        {
         for (int i = 0; i < n_x; ++i)
         {
-          reordered_M[(r + j * size) * n_x + i] = global_M[(r * max_rows + j) * n_x + i];
+          reordered_M[(r + j * size) * n_x + i] = global_M[(r * local_rows + j + (r < remaining_rows ? remaining_rows : 0)) * n_x + i];
         }
        }
       }
